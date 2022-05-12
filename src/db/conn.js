9c1beee3b2da67b2f17a4e8df6acb6a8652564c3
felipemacedo315 <felipemacedo315@gmail.com>
2022-05-12 00:38:15 -0300

    Setup inicial para o projeto feito

---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const {Sequelize} = require('sequelize')
+const conn = new Sequelize('toughts','root','',{
+  dialect:'mysql',
+  host:'localhost',
+  logging:console.log('Conection is work')
+})
+
+
+
+module.exports=conn
+
+
