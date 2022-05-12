b737b6175b11e883abeb8523da41e6ed764b3e5c
felipemacedo315 <felipemacedo315@gmail.com>
2022-05-12 01:45:39 -0300

    Header do projeto feito, junto com css

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,8 @@
-const {Sequelize} = require('sequelize')
-const conn = new Sequelize('toughts','root','',{
-  dialect:'mysql',
-  host:'localhost',
-  logging:console.log('Conection is work')
-})
+const { Sequelize } = require("sequelize");
+const conn = new Sequelize("toughts", "root", "", {
+  dialect: "mysql",
+  host: "localhost",
+  logging: console.log("Conection is work"),
+});
 
-
-
-module.exports=conn
-
-
+module.exports = conn;
