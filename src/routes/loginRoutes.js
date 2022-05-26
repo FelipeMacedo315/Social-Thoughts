0733927fbb8eeed74aa0ce02ceea5d52d8fee298
felipemacedo315 <felipemacedo315@gmail.com>
2022-05-26 11:37:52 -0300

    rota principal adicionada e msg na dashboard quando o usuario não tem nenhum pensamento

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -4,7 +4,7 @@ const routesLogin = express.Router();
 
 routesLogin.use(express.urlencoded({extended: true}));
 
-routesLogin.get("/Login", loginGet);
+routesLogin.get("/", loginGet);
 routesLogin.post("/LoginPost", loginPost);
 
 module.exports = routesLogin;
